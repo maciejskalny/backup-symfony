4c2a85e6a5106c1e7fc0be4ace38fb38755a9016
Virtua <m.skalny@wearevirtua.com>
2018-08-21 16:39:27 +0200

    VIMS-43

--- my-project/src/Controller/ImportExportController.php
+++ my-project/src/Controller/ImportExportController.php
@@ -11,10 +11,15 @@
 namespace App\Controller;
 
 use App\Entity\ProductCategory;
+use App\Form\ImportCategoryType;
 use App\Service\CsvActions;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * Class ImportExportController
@@ -24,11 +29,43 @@ use Symfony\Component\HttpFoundation\Response;
 class ImportExportController extends Controller
 {
     /**
+     * @Route("/categories/import", name="categories_import")
+     */
+    public function importCategories(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $form = $this->createForm(ImportCategoryType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $serializer = new Serializer([new ObjectNormalizer()], [new CsvEncoder()]);
+            $data = $serializer->decode(file_get_contents($form->get('importFile')->getData()), 'csv');
+            foreach ($data as $row)
+            {
+               
+                $category = new ProductCategory();
+                $category->setName($row['name']);
+                $category->setDescription($row['description']);
+                $category->setAddDate($row['created_at']);
+                $category->setLastModifiedDate($row['last_modified']);
+
+                $em->persist($category);
+                $em->flush();
+            }
+        }
+
+        return $this->render('product_category/import.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
      * @return Response
      * @param CsvActions $csvActionsService
-     * @Route("/categories", name="categories_export")
+     * @Route("/categories/export", name="categories_export")
      */
-    public function exportCategoriesToCsv(CsvActions $csvActionsService)
+    public function exportCategories(CsvActions $csvActionsService)
     {
         $categories = $this->getDoctrine()->getRepository(ProductCategory::class)->findAll();
         $data = [];
