e8ff8504342b0fd86b3bb97b7748a41f39b1693d
Virtua <m.skalny@wearevirtua.com>
2018-08-24 13:05:29 +0200

    VIMS-52

--- my-project/public/js/display-type.js
+++ my-project/public/js/display-type.js
@@ -1,13 +1,38 @@
-$(document).ready(function () {
-    $('#display-type').click(function () {
-        let choice = $('#display-value').val();
+// $(document).ready(function () {
+//     $('#display-type').click(function () {
+//         let choice = $('#display-value').val();
+//
+//         if(choice == 2) {
+//             $('.product').removeClass('col-6');
+//             $('.product').addClass('col-4');
+//         } else {
+//             $('.product').removeClass('col-4');
+//             $('.product').addClass('col-6');
+//         }
+//     });
+// });
 
-        if(choice == 2) {
-            $('.product').removeClass('col-12');
-            $('.product').addClass('col-4');
-        } else {
+$(document).ready(function () {
+    $('.display-type').click(function () {
+
+        var lastClass = $('.product').attr('class').split(' ').pop();
+        $(this).removeClass(lastClass);
+
+        let choice = $(this).attr('id');
+
+        if(choice == 1) {
+            $('.product').removeClass('col-6');
             $('.product').removeClass('col-4');
             $('.product').addClass('col-12');
+        } else if (choice == 2) {
+            $('.product').removeClass('col-12');
+            $('.product').removeClass('col-4');
+            $('.product').addClass('col-6');
+        } else {
+            $('.product').removeClass('col-6');
+            $('.product').removeClass('col-12');
+            $('.product').addClass('col-4');
         }
+
     });
 });
